93c602cdbb1caf400124fff8927fab588eb3f9b9
Engin Eken <engin.eken@trendyol.com>
2022-11-17 13:13:09 +0300

    device jobs test first commit

--- jobs/interface_jobs.py
+++ jobs/interface_jobs.py
@@ -18,6 +18,7 @@ from nautobot.extras.choices import (
     SecretsGroupSecretTypeChoices,
 )
 from nautobot.extras.secrets.exceptions import SecretError
+from nautobot.dcim.filters import DeviceFilterSet
 
 from django.conf import settings
 from django.core.exceptions import ObjectDoesNotExist
@@ -25,6 +26,56 @@ from django.core.exceptions import ObjectDoesNotExist
 from device_filter import filter_devices
 import socket
 import re
+
+
+def filter_devices(data):
+    """
+    * Getting TreeQuerySet and RestrictedQuerySet values as data chosen from FormEntry and
+    filter all devices based on data values.
+    * Filtering can be done with AND or OR operator based on the selecetion of filter_type.
+    """
+    FIELDS = {
+        "tenant_group",
+        "tenant",
+        "region",
+        "site",
+        "rack",
+        "rack_group",
+        "role",
+        "platform",
+        "device_type",
+        "status",
+        "manufacturer",
+        "tags",
+    }
+    query = {}
+    for field in FIELDS:
+        if data.get(field):
+            query[f"{field}_id"] = data[field].values_list("pk", flat=True)
+    # Handle case where object is from single device run all.
+    if data.get("device") and isinstance(data["device"], Device):
+        query.update({"id": [str(data["device"].pk)]})
+    elif data.get("device"):
+        query.update({"id": data["device"].values_list("pk", flat=True)})
+
+    base_qs = Device.objects.all()
+
+    if base_qs.count() == 0:
+        raise Exception(
+            "The base queryset didn't find any devices. Please check the Setting scope."
+        )
+    devices_filtered = DeviceFilterSet(data=query, queryset=base_qs)
+    if devices_filtered.qs.count() == 0:
+        raise Exception(
+            "The provided job parameters didn't match any devices detected by the scope. Please check the scope defined within Settings or select the correct job parameters to correctly match devices."
+        )
+    devices_no_platform = devices_filtered.qs.filter(platform__isnull=True)
+    if devices_no_platform.count() > 0:
+        raise Exception(
+            f"The following device(s) {', '.join([device.name for device in devices_no_platform])} have no platform defined. Platform is required."
+        )
+
+    return devices_filtered.qs
 
 
 class FormEntry:
