65fa7deb89807e98fb1604e5963f7ce9dc5b8c47
Engin Eken <engin.eken@trendyol.com>
2022-11-17 13:33:43 +0300

    device jobs test first commit

--- jobs/device_jobs.py
+++ jobs/device_jobs.py
@@ -7,7 +7,6 @@ class NapalmGetJob(Job):
     class Meta:
         name = "Device Remove Flow Job"
         description = "Device removal"
-        has_sensitive_variables = False
     
     serial = StringVar()
 
