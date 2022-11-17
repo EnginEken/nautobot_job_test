e2da34d2bedb07de21647dc4a3563389f39e3958
Engin Eken <engin.eken@trendyol.com>
2022-11-17 13:16:54 +0300

    device jobs test first commit

--- jobs/device_jobs.py
+++ jobs/device_jobs.py
@@ -1,4 +1,30 @@
+from nautobot.dcim.models import (
+    Device,
+    DeviceType,
+    Manufacturer,
+    Region,
+    RackGroup,
+    Rack,
+    Site,
+    DeviceRole,
+    Platform,
+    Interface,
+)
+from nautobot.tenancy.models import TenantGroup, Tenant
+from nautobot.extras.models import Status, Tag, CustomField
 from nautobot.extras.jobs import *
+from nautobot.extras.choices import (
+    SecretsGroupAccessTypeChoices,
+    SecretsGroupSecretTypeChoices,
+)
+from nautobot.extras.secrets.exceptions import SecretError
+from nautobot.dcim.filters import DeviceFilterSet
+
+from django.conf import settings
+from django.core.exceptions import ObjectDoesNotExist
+
+import socket
+import re
 
 from device_filter import filter_devices
 
