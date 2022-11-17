636420f84fea744729a641d9e22cf75f31d73682
Engin Eken <engin.eken@trendyol.com>
2022-11-17 11:18:34 +0300

    device jobs test first commit

--- jobs/device_jobs.py
+++ jobs/device_jobs.py
@@ -33,32 +33,8 @@ def filter_devices(data):
     filter all devices based on data values.
     * Filtering can be done with AND or OR operator based on the selecetion of filter_type.
     """
-    FIELDS = {
-        "serial",
-    }
-    query = {}
-    for field in FIELDS:
-        if data.get(field):
-            query[f"{field}"] = data[field]
-
-    base_qs = Device.objects.all()
-
-    if base_qs.count() == 0:
-        raise Exception(
-            "The base queryset didn't find any devices. Please check the Setting scope."
-        )
-    devices_filtered = DeviceFilterSet(data=query, queryset=base_qs)
-    if devices_filtered.qs.count() == 0:
-        raise Exception(
-            "The provided job parameters didn't match any devices detected by the scope. Please check the scope defined within Settings or select the correct job parameters to correctly match devices."
-        )
-    # devices_no_platform = devices_filtered.qs.filter(platform__isnull=True)
-    # if devices_no_platform.count() > 0:
-    #     raise Exception(
-    #         f"The following device(s) {', '.join([device.name for device in devices_no_platform])} have no platform defined. Platform is required."
-    #     )
-
-    return devices_filtered.qs
+    devices_filtered = Device.objects.filter(serial='1B5VJQ3')
+    return devices_filtered
 
 
 class NapalmGetJob(Job):
