c04dabda16dd3d75208b2e054fdc928d3da65263
Engin Eken <engin.eken@trendyol.com>
2022-11-17 13:34:51 +0300

    device jobs test first commit

--- jobs/device_jobs.py
+++ jobs/device_jobs.py
@@ -1,16 +1,16 @@
-from nautobot.extras.jobs import *
+# from nautobot.extras.jobs import *
 
-from device_filter import filter_devices
+# from device_filter import filter_devices
 
 
-class NapalmGetJob(Job):
-    class Meta:
-        name = "Device Remove Flow Job"
-        description = "Device removal"
+# class NapalmGetJob(Job):
+#     class Meta:
+#         name = "Device Remove Flow Job"
+#         description = "Device removal"
     
-    serial = StringVar()
+#     serial = StringVar()
 
-    def run(self, data, commit):
-        query, devices = filter_devices(data)
-        self.log_warning(message=query)
-        return devices
+#     def run(self, data, commit):
+#         query, devices = filter_devices(data)
+#         self.log_warning(message=query)
+#         return devices
